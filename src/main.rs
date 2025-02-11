5d09927046fdae3c3175bb51786275cf7f541849
georgephilippas-ber <george.philippas@outlook.com.gr>
2025-02-11 20:26:41 +0200

    First commit: a symphony of potential, played entirely in debug mode.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use crate::database::create_postgres_pool;
 use crate::facets::surveying::Surveying;
 
@@ -6,10 +5,7 @@ mod database;
 mod facets;
 
 fn solve(input: Vec<i32>) -> Vec<i32> {
-    let g = input
-        .iter()
-        .enumerate()
-        .map(|(i, x)| (x, i as i32));
+    let g = input.iter().enumerate().map(|(i, x)| (x, i as i32));
 
     g.keys()
 }
