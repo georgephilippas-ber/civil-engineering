b2355f1ebf668937bced8f114a89fb1a7555221f
George Philippas <george.philippas@outlook.com.gr>
2025-02-15 11:33:42 +0200

    First commit: a symphony of potential, played entirely in debug mode.

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,5 +1,15 @@
 use sqlx::{Error, PgPool};
 
+static USERNAME: &str = "postgres";
+static PASSWORD: &str = "development";
+
+fn postgres_connection_string() -> String {
+    format!(
+        "postgres://{}:{}@localhost:5432/development",
+        USERNAME, PASSWORD
+    )
+}
+
 pub async fn create_postgres_pool() -> Result<PgPool, Error> {
-    PgPool::connect("postgres://development:development@localhost:5432/development").await
+    PgPool::connect(postgres_connection_string().as_str()).await
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,6 @@ use crate::facets::surveying::Surveying;
 mod database;
 mod facets;
 
-fn solve(input: Vec<i32>) -> Vec<i32> {
-    let g = input.iter().enumerate().map(|(i, x)| (x, i as i32));
-
-    g.keys()
-}
 
 #[tokio::main]
 async fn main() {
